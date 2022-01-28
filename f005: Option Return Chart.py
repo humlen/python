8300ef17065c41ef68814a84951c98ca7510de67
Eirik Humlen <95056805+humlen@users.noreply.github.com>
2022-01-28 22:50:51 +0100

    Update f005: Option Return Chart.py

--- f005: Option Return Chart.py
+++ f005: Option Return Chart.py
@@ -3,9 +3,52 @@ import matplotlib.pyplot as plt # Using plt over seaborn due to wide data pref
 import numpy as np
 import pandas as pd
 
+
+# LONG CALLS
 # Stock Info
-current_price = 50.59 # Current price of the security
-strike = 55 # Strike price of the option 
+current_price = 46.73# Current price of the security
+strike = 25 # Strike price of the option 
+premium = 22.0565 # Premium issued at the sale of an option at the strike
+stock_range_min = 5 # Lowest stock value to model for 
+stock_range_max = 200 # Highest stock value to model for 
+step = 0.1 # Steps in the calculation
+
+custom_range = np.arange(stock_range_min,stock_range_max,step)
+
+# Return Calculator
+buy_hold = []
+long_call = []
+for i in custom_range:
+    buy_and_hold = 100*(i-current_price)
+    buy_hold.append(buy_and_hold)
+    if i >= strike:
+        lc = 100*(i-(strike+premium))
+    if i < strike:
+        lc = 100*(-premium)
+    long_call.append(lc)
+
+# Create Dataframe
+tuplelist = list(zip(custom_range,buy_hold,long_call))
+df = pd.DataFrame(tuplelist, columns = ['Stock Price', 'Buy & Hold','Long Calls']) # Not sure of a quicker way to turn lists into a df
+
+# Return Chart
+plt.plot(df['Stock Price'], df['Buy & Hold'], label = "Buy & Hold")
+plt.plot(df['Stock Price'], df['Long Calls'], label = "Long Calls")
+plt.axhline(0, color='black', linestyle=':') # Line for the 0-return indicator
+plt.axvline(current_price, color = 'black', linestyle = ':') # Line for the current price indicator
+plt.title('Option Return Chart')
+plt.xlabel('Stock Price')
+plt.ylabel('Absolute Return')
+plt.legend()
+plt.yscale('linear') # Can turn to symlog, but hurts readability for others
+plt.show()
+
+
+"""
+# SHORT COVERED CALLS
+# Stock Info
+current_price = 47.73 # Current price of the security
+strike = 25 # Strike price of the option 
 premium = 0.47 # Premium issued at the sale of an option at the strike
 stock_range_min = 45 # Lowest stock value to model for 
 stock_range_max = 60 # Highest stock value to model for 
@@ -40,4 +83,4 @@ plt.xlabel('Stock Price')
 plt.ylabel('Absolute Return')
 plt.legend()
 plt.yscale('linear') # Can turn to symlog, but hurts readability for others
-plt.show()
+plt.show() """
